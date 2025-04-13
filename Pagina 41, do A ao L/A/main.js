e82644eeaeff52df51122cc2670cba2878b4a5e2
Pedro Santana <pedro.santana9905@gmail.com>
2025-04-13 05:08:54 -0300

    up 4

--- Pagina 41, do A ao L/A/main.js
+++ Pagina 41, do A ao L/A/main.js
@@ -1,2 +1,13 @@
 
+let valor1 = parseInt(prompt("Digite o primeiro valor"))
+let valor2 = parseInt(prompt("Digite o segundo valor"))
 
+let diferencaEntreValores
+
+if(valor1 > valor2) {
+    diferencaEntreValores = valor1 - valor2
+} else {
+    diferencaEntreValores = valor2 - valor1
+}
+
+alert(`A diferença entre os valores é de ${diferencaEntreValores}`)
---
+++ Pagina 41, do A ao L/B/main.js
@@ -0,0 +1,8 @@
+
+let numero = parseInt(prompt("Digite um valor positivo ou negativo"))
+
+if (numero < 0) {
+    numero = numero * -1
+}
+
+alert("O número digitado é "+ numero)
